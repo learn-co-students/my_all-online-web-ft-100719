d8a980a6ea65baa3880ea77048639022ead531b6
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-12 04:25:17 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i=0
+  block_return_values = []
+  while i < collection.length
+    block_return_values<< yield(collection[i])# when code gets run here, it send whatever is passed in as argument to the block. It gets evaluated, and the return value is passed into the new array.
+    i+=1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
